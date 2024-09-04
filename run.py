e6b3b6a5779b631b4fd508e7dcffaffa9a6a3817
Abdallah0799 <tornadogazal@gmail.com>
2024-09-04 18:32:48 +0200

    rate limiting

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import os
 
 from actions.create_new_product import run
@@ -6,8 +8,16 @@ from actions.create_new_product import run
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
 
+# Initialize the Limiter
+limiter = Limiter(
+    get_remote_address,  # Use the remote IP address of the request as the key for rate limiting
+    app=app,
+    default_limits=["100 per hour"]  # Default limit if no other specific limit is defined
+)
+
 
 @app.route("/get_new_product", methods=["GET"])
+@limiter.limit("5 per minute")  # Limit this specific route to 10 requests per minute
 def get_new_product():
     try:
         product = run()
